c1f1181e116fd86d3c4db5f91e6995a62a32d6e5
Thomas Robillard <thomas.robillard@trojans.dsu.edu>
2025-05-31 09:45:40 -0500

    testing various C# types

---
+++ Chapter 4/Types/Types/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myInt = 10;
+        byte myByte = (byte)myInt;
+        double myDouble = (double)myByte;
+        //bool myBool = (bool)myDouble;
+        string myString = "false";
+        //myBool = (bool)myString;
+        //myString = (string)myInt;
+        myString = myInt.ToString();
+        //myBool = (bool)myByte;
+        //myByte = (byte)myBool;
+        short myShort = (short)myInt;
+        char myChar = 'x';
+        //myString = (string)myChar;
+        long myLong = (long)myInt;
+        decimal myDecimal = (decimal)myLong;
+        myString = myString + myInt + myByte + myDouble + myChar;
+        Console.WriteLine(myString);
+
+    }
+    public int MyMethod(bool add3)
+    {
+        int value = 12;
+
+        if (add3)
+            value += 3;
+        else
+            value -= 2;
+        return value;
+    }
+}
