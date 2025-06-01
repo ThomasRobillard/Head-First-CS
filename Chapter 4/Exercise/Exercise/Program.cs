65c4714d7fc7da881493394e554bbd23380df3fa
Thomas Robillard <thomas.robillard@trojans.dsu.edu>
2025-06-01 11:27:59 -0500

    Completed elephant exercise

---
+++ Chapter 4/Exercise/Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+        Elephant swap;
+        Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap. q to quit");
+        while (true)
+        {
+            char keyChar = Console.ReadKey(true).KeyChar;
+
+            if (keyChar == '1')
+            {
+                Console.WriteLine("You pressed 1");
+                Console.WriteLine("Calling lloyd.WhoAmI()");
+                lloyd.WhoAmI();
+            }
+            else if (keyChar == '2')
+            {
+                Console.WriteLine("You pressed 2");
+                Console.WriteLine("Calling lucinda.WhoAmI()");
+                lucinda.WhoAmI();
+            }
+            else if (keyChar == '3')
+            {
+                Console.WriteLine("You pressed 3");
+                swap = lucinda;
+                lucinda = lloyd;
+                lloyd = swap;
+                Console.WriteLine("References have been swapped");
+            }
+            else if ((keyChar == 'q') || (keyChar == 'Q'))
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap. q to quit");
+            }
+        }
+    }
+}
